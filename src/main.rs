9427ae9a504b6a05d36f3f74d3d2f1194ce00eb5
Daniel Moreira Yokoyama <792153+moreirayokoyama@users.noreply.github.com>
2024-06-21 16:56:19 -0300

    Criando a Recepção

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,52 @@
-use bevy::app::{App, Update};
+use bevy::app::{App, Startup, Update};
+use bevy::ecs::component::Component;
+use bevy::prelude::{Commands, Query};
+use bevy::time::{Timer, TimerMode};
+use rand::{thread_rng, Rng};
+
+#[derive(Component)]
+struct Funcionario {
+    esta_livre: bool,
+}
+
+#[derive(Component)]
+struct Recepcionista;
+
+#[derive(Component)]
+struct Atendente;
+
+#[derive(Component)]
+struct Cozinheiro;
+
+#[derive(Component)]
+struct Recepcao
+{   
+    aberta: bool,
+    timer: Timer,
+}
 
 fn main() {
     App::new()
-        .add_systems(Update, print_helloworld)
+        .add_systems(Startup, iniciar_restaurante)
+        .add_systems(Update, eventos_recepcao)
         .run();
 }
 
-fn print_helloworld() {
-    println!("Olá mundo");
+fn iniciar_restaurante(mut commands: Commands) {
+    commands.spawn((Funcionario { esta_livre: true }, Recepcionista));
+    commands.spawn((Funcionario { esta_livre: true }, Atendente));
+    commands.spawn((Funcionario { esta_livre: true }, Cozinheiro));
+    commands.spawn((Funcionario { esta_livre: true }, Cozinheiro));
+    commands.spawn(Recepcao {aberta: false, timer: Timer::from_seconds(30., TimerMode::Once)});
+}
+
+fn eventos_recepcao(mut query: Query<&mut Recepcao>) {
+    let mut rng = thread_rng();
+    for mut recepcao in &mut query {
+        if recepcao.timer.finished() {
+            println!("Evento da Recepção");
+            recepcao.aberta = true;
+            recepcao.timer = Timer::from_seconds(rng.gen(), TimerMode::Once)
+        }
+    }
 }
