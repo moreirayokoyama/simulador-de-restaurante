4fabe52ba88ac2a070977c97d67a86e1c7f3927b
Daniel Moreira Yokoyama <792153+moreirayokoyama@users.noreply.github.com>
2024-06-29 10:46:48 -0300

    Criação do Plugin da mesa

--- src/mesa/mod.rs
+++ src/mesa/mod.rs
@@ -1,7 +1,18 @@
-use bevy::prelude::Component;
+use bevy::{app::{Plugin, Update}, prelude::Component};
 
 #[derive(Component)]
 pub struct Mesa {
     pub ocupada: bool,
 }
 
+pub struct MesaPlugin;
+
+impl Plugin for MesaPlugin {
+    fn build(&self, app: &mut bevy::prelude::App) {
+        app.add_systems(Update, criar_pedidos)
+    }
+}
+
+fn criar_pedidos() {
+
+}
